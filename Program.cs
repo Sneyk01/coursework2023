99569147e6dce8c6091e550eb29db214013b88b3
Sneyk01 <90918010+Sneyk01@users.noreply.github.com>
2023-12-09 15:30:35 +0300

    Update form names

--- Program.cs
+++ Program.cs
@@ -13,8 +13,14 @@ namespace myPongGame
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
 
+            foreach (var f in new DirectoryInfo(@"...").GetFiles("*.cs", SearchOption.AllDirectories))
+            {
+                string s = File.ReadAllText(f.FullName);
+                File.WriteAllText(f.FullName, s, Encoding.UTF8);
+            }
+
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new mainForm());
         }
     }
 }
---
+++ nameForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myPongGame
+{
+    public partial class nameForm : Form
+    {
+        public String _userName;
+        public nameForm()
+        {
+            InitializeComponent();
+            this._userName = "";
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            String userName = nameBox.Text;
+            if (userName.Length < 3)
+            {
+                errorLabel.Visible = true;
+            }
+            else
+            {
+                this._userName = nameBox.Text;
+                DialogResult = DialogResult.OK;
+                this.Close();
+            }
+        }
+    }
+}
